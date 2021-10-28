b7b20fe104d00b83dfaed58792b343edb89f5be0
Amehra2 <akashmehra2001@hotmail.com>
2021-10-28 10:49:38 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -16,4 +16,41 @@ function App(){
   const [name, setname] = React.useState('');
 
   const [startDate, setstartDate] = React.useState('');
-}
+
+  function showClickHandler () {
+    window.alert("[" + "id = "+ treatId + ", treatCourseId = "+treatCourseId+ ", type = "+ type + ", category = "+category + " , name = "+name+ " , startDate = "+startDate + "]");
+  
+  }
+
+  return(
+    <div>
+
+        <input placeholder="treatId" value={treatId}  onChange = { e => settreatId(e.target.value)}  ></input>
+
+        <br/>
+
+        <input placeholder="treatCourseId" value={treatCourseId}  onChange = { e => settreatCourseId(e.target.value)}  ></input>
+
+        <br/>
+
+        <input placeholder="type" value={type}  onChange = { e => settype(e.target.value)}  ></input>
+        
+        <br/>
+
+        <input placeholder="category" value={category}  onChange = { e => setcategory(e.target.value)}  ></input>
+
+        <br/>
+
+        <input placeholder="name" value={name}  onChange = { e => setname(e.target.value)}  ></input>
+
+        <br/>
+
+        <input placeholder="startDate" value={startDate}  onChange = { e => startDate(e.target.value)}  ></input>
+
+        </div>
+    );
+
+
+  }
+  
+  export default App;
