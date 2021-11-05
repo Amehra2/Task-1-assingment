43fa8b485a9cd8004824cbc1853db829474b3b78
Amehra2 <akashmehra2001@hotmail.com>
2021-11-05 13:27:23 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -17,20 +17,14 @@ const startDate1 = "01-01-2020";
  export default function App(){ // Here below, I have created the identifications for the elements that are included in the instance above. This relates to the props assigned in the MedicalTreatment.js component. Based on them, here below I am declaring and assigning them a specific identification. This allows us to create constants, which have been done above.
    return(
 
-<div> 
-  <MedicalTreatment treatId={treatId1} treatCourseId={treatCourseId1} type={type1}  category={category1} name={name1} startDate={startDate1}/>
-  
+<div>   
   <p>Treat ID: {treatId1}</p>
   <p>TreatCourseId: {treatCourseId1}</p>
   <p>Type: {type1}</p>
   <p>Category: {category1} </p>
   <p>Name: {name1}</p>
-  <p> startDate: {startDate1}</p>
+  <p> StartDate: {startDate1}</p>
 
-  
-  
-  
-  
   </div>
    )
  }
