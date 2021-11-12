b1ba8c232a9fb9e3840cfa4150803c0319152bd3
Amehra2 <akashmehra2001@hotmail.com>
2021-11-12 10:15:04 +0000

    3 files modified

--- src/MedicalTreatment.js
+++ src/MedicalTreatment.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./style.css";
 
-export default function Medicaltreatment (props) {  //Here I am creating and declaring a new function which is called MedicalTreatment. Within this function, I have created props, which we can see below. These props we will use in App.js to retrieve and set values for the medical record.
+export default function Medicaltreatment (props) { 
   return (
       <div> 
     {props.treatId} {props.treatCourseId} {props.type} {props.category} {props.name} {props.startDate} 
--- src/MedicalTreatmentList.js
+++ src/MedicalTreatmentList.js
@@ -1,19 +1,34 @@
-import React from 'react';//Task 2
+import React from 'react';
 import MedicalTreatment from './MedicalTreatment.js'
 
 
 function MedicalTreatmentList (props){
-  return(
-    <li key= {props.treatId} > {props.treatId} {props.treatCourseId} {props.type} {props.category} {props.name} {props.startDate} </li>
 
-  );
+    const MedicalTreatmentListJson = '[{"treatId": LHM402, "treatCourseId":"02022", "type": "long term", "category": "dementia", "name": "Joseph stevens", "startDate": "01-01-2020"} ,' +
+
+    '[{"treatId": LHM403, "treatCourseId":"02023", "type": "short term", "category": "knee injury", "name": "James Waterstone", "startDate": "25-04-2020} ,' +
+
+    '[{"treatId": LHM404, "treatCourseId":"02024", "type": "long term", "category": "epilepsy", "name": "Sarah Mcsweeny", "startDate": "17-01-2021"} ,' +
+
+    '[{"treatId": LHM405, "treatCourseId":"02025", "type": "long term", "category": "heart patient", "name": "Steve Maxime", "startDate": "19-08-2019"} ,' +
+
+    '[{"treatId": LHM406, "treatCourseId":"02026", "type": "short term", "category": "hamstring injury", "name": "Amir Raheem", "startDate": "11-11-2019"}]';
+
+    const MedicalTreatmentList = (JSON.parse(MedicalTreatmentListJson));
+
+    const treatmentlist = MedicalTreatmentList.map(
+      (MedicalTreatment) => <MedicalTreatment treatId= {MedicalTreatment.treatId} treatCourseId= {MedicalTreatment.treatCourseId} type={MedicalTreatment.type} category={MedicalTreatment.category} name={MedicalTreatment.name} startDate={MedicalTreatment.startDate}/>);
+
+      return (
+        <div>
+          <ul>{treatmentlist}</ul>
+          </div>
+
+      );
+    
+    //li key= {props.treatId} > {props.treatId} {props.treatCourseId} {props.type} {props.category} {props.name} {props.startDate} 
+
+  
 }
-
-const treatmentList = treatments.map(
-
-)
-
-
-
 
 export default MedicalTreatmentList;
